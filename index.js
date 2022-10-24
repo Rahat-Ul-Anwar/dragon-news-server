09e357aaad5828ae13029b98acf260f6872e3825
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-10-24 12:13:25 +0600

    dragon news server side added

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+const port = process.env.Port || 5000;
+
+const categories = require("./data/categories.json");
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('News API Running');
+    
+})
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories);
+
+})
+
+app.listen(port, () => {
+    console.log('Dragon News server running on Port', port);
+})
